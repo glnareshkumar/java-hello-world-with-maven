fbd10eb2b5721f83f98bddf8f4957c4ccb435c65
glnareshkumar <glnareshkumar@gmail.com>
2022-05-14 13:49:06 +0530

    pollscm2test

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,7 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Welcome to DevOps";
-	return "Naresh01";
+        return "Welcome to DevOps Naresh";
     }
 }
