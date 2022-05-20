fcc96be3a06cf3403e8c045b6745d09073903b78
glnareshkumar <glnareshkumar@gmail.com>
2022-05-21 00:51:09 +0530

    hellossh

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Welcome to DevOps Nareshkumar";
+        return "connect through ssh Welcome to DevOps Nareshkumar";
     }
 }
